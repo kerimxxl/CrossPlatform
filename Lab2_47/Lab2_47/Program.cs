3714c9e21ade26f4498b6dd5ed5c43ce90817ab6
Kerim <51818857+kerimxxl@users.noreply.github.com>
2022-10-18 19:03:48 +0200

    Add files via upload
    done

---
+++ Lab2_47/Lab2_47/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab2_47
+{
+    public class Program
+    {
+        public static string InputFilePath = @"..\..\input.txt";
+        public static string OutputFilePath = @"..\..\output.txt";
+
+        static void Main(string[] args)
+        {
+            FileInfo outputFileInfo = new FileInfo(OutputFilePath);
+            var numberOfTrees = Convert.ToInt32(File.ReadLines(InputFilePath).First().Trim());
+            using (StreamWriter streamWriter = outputFileInfo.CreateText())
+            {
+                if (numberOfTrees < 1 || numberOfTrees > 50)
+                {
+                    streamWriter.WriteLine("Number is out of range");
+                }
+                else
+                {
+                    streamWriter.WriteLine(3 * Math.Pow(2, numberOfTrees - 1));
+                }
+            }
+        }
+    }
+}
