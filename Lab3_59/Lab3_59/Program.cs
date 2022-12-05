826ca4a0e596fb1496a6b3fafe471889349ecaba
Kerim <51818857+kerimxxl@users.noreply.github.com>
2022-12-05 14:35:23 +0100

    Add files via upload
    Lab3 added

---
+++ Lab3_59/Lab3_59/Program.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Lab3_59
+{
+    /// <summary>
+    /// Cторона, по відношенню до ігрової карти, куди гравець дивиться
+    /// </summary>
+    enum Side
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+    public class Program
+    {
+        public static string InputFilePath = @"..\..\input.txt";
+        public static string OutputFilePath = @"..\..\output.txt";
+
+        static void Main(string[] args)
+        {
+            FileInfo outputFileInfo = new FileInfo(OutputFilePath);
+            var inputData = File.ReadLines(InputFilePath);
+            var buf = inputData.First().Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int k = Convert.ToInt32(buf[0]), n = Convert.ToInt32(buf[1]), m = Convert.ToInt32(buf[2]);
+            var game = inputData.Skip(1).Select(row => row.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => Convert.ToInt32(x)).ToArray()).ToArray();
+
+            using (StreamWriter streamWriter = outputFileInfo.CreateText())
+            {
+                if (k < 0 || k > 5 || n < 1 || n > 20 || m < 1 || m > 20)
+                {
+                    streamWriter.WriteLine("Out of range exception!");
+                }
+                else
+                {
+                    streamWriter.WriteLine(GetMinTime(game, k));
+                }
+            }
+        }
+
+        public static int GetMinTime(int[][] game, int rightCount)
+        {
+            (int row, int col) startPos = (-1, -1);
+            for (int i = 0; i < game.Length; i++)
+            {
+                for (int j = 0; j < game[0].Length; j++)
+                {
+                    if (game[i][j] == 2)
+                    {
+                        startPos = (i, j);
+                        break;
+                    }
+                }
+                if (startPos.row != -1)
+                {
+                    break;
+                }
+            }
+            if (startPos.row == -1)
+            {
+                return -1;
+            }
+            else
+            {
+                List<int> timeList = new List<int>();
+                timeList.Add(getMinTime(startPos, Side.Up, game, rightCount));
+                timeList.Add(getMinTime(startPos, Side.Down, game, rightCount));
+                timeList.Add(getMinTime(startPos, Side.Left, game, rightCount));
+                timeList.Add(getMinTime(startPos, Side.Right, game, rightCount));
+                timeList = timeList.Where(time => time != -1).ToList();
+                return timeList.Any() ? timeList.Min() : -1;
+            }
+        }
+
+        private static List<Tuple<int, int, Side, int>> gameBuf = new List<Tuple<int, int, Side, int>>();
+        private static int getMinTime((int row, int col) currPos, Side currWatchSide, int[][] game, int rightCount, int time = -1)
+        {
+            if (gameBuf.Contains(new Tuple<int, int, Side, int>(currPos.row, currPos.col, currWatchSide, rightCount)))
+            {
+                return -1;
+            }
+            gameBuf.Add(new Tuple<int, int, Side, int>(currPos.row, currPos.col, currWatchSide, rightCount));
+            time++;
+            if (game[currPos.row][currPos.col] == 3)
+            {
+                return time;
+            }
+            List<((int row, int col) nextPos, Side nextWatchSide, bool usedRight)> whereToGoList = getPosToGoList(currPos, currWatchSide, game, rightCount > 0);
+            List<int> timeList = new List<int>();
+            foreach (var whereToGo in whereToGoList)
+            {
+                timeList.Add(getMinTime(whereToGo.nextPos, whereToGo.nextWatchSide, game, whereToGo.usedRight ? rightCount - 1 : rightCount, time));
+            }
+            timeList = timeList.Where(time => time != -1).ToList();
+            return timeList.Any() ? timeList.Min() : -1;
+        }
+
+        private static List<((int row, int col) nextPos, Side nextWatchSide, bool usedRight)> getPosToGoList((int row, int col) currPos, Side currWatchSide, int[][] game, bool hasRightSide)
+        {
+            List<((int row, int col), Side, bool)> result = new List<((int row, int col), Side, bool)>();
+            switch (currWatchSide)
+            {
+                case Side.Up:
+                    if (currPos.row != 0 && game[currPos.row - 1][currPos.col] != 1)
+                    {
+                        result.Add(((currPos.row - 1, currPos.col), Side.Up, false));
+                    }
+                    if (currPos.col != 0 && game[currPos.row][currPos.col - 1] != 1)
+                    {
+                        result.Add(((currPos.row, currPos.col - 1), Side.Left, false));
+                    }
+                    if (hasRightSide && currPos.col != game[0].Length - 1 && game[currPos.row][currPos.col + 1] != 1)
+                    {
+                        result.Add(((currPos.row, currPos.col + 1), Side.Right, true));
+                    }
+                    break;
+                case Side.Down:
+                    if (currPos.row != game.Length - 1 && game[currPos.row + 1][currPos.col] != 1)
+                    {
+                        result.Add(((currPos.row + 1, currPos.col), Side.Down, false));
+                    }
+                    if (hasRightSide && currPos.col != 0 && game[currPos.row][currPos.col - 1] != 1)
+                    {
+                        result.Add(((currPos.row, currPos.col - 1), Side.Left, true));
+                    }
+                    if (currPos.col != game[0].Length - 1 && game[currPos.row][currPos.col + 1] != 1)
+                    {
+                        result.Add(((currPos.row, currPos.col + 1), Side.Right, false));
+                    }
+                    break;
+                case Side.Left:
+                    if (currPos.col != 0 && game[currPos.row][currPos.col - 1] != 1)
+                    {
+                        result.Add(((currPos.row, currPos.col - 1), Side.Left, false));
+                    }
+                    if (currPos.row != game.Length - 1 && game[currPos.row + 1][currPos.col] != 1)
+                    {
+                        result.Add(((currPos.row + 1, currPos.col), Side.Down, false));
+                    }
+                    if (hasRightSide && currPos.row != 0 && game[currPos.row - 1][currPos.col] != 1)
+                    {
+                        result.Add(((currPos.row - 1, currPos.col), Side.Down, true));
+                    }
+                    break;
+                case Side.Right:
+                    if (currPos.row != 0 && game[currPos.row - 1][currPos.col] != 1)
+                    {
+                        result.Add(((currPos.row - 1, currPos.col), Side.Up, false));
+                    }
+                    if (hasRightSide && currPos.row != game.Length - 1 && game[currPos.row + 1][currPos.col] != 1)
+                    {
+                        result.Add(((currPos.row + 1, currPos.col), Side.Down, true));
+                    }
+                    if (currPos.col != game[0].Length - 1 && game[currPos.row][currPos.col + 1] != 1)
+                    {
+                        result.Add(((currPos.row, currPos.col + 1), Side.Right, false));
+                    }
+                    break;
+            }
+            return result;
+        }
+    }
+}
